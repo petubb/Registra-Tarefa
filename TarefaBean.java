97d38be0c47dd4dd11a9b0ed4e196915a4eeb4b7
petub <114745589+petubb@users.noreply.github.com>
2022-11-07 06:48:05 -0400

    tentativa de um registro de tartefa
    tentativa de um registro de tartefa

---
+++ TarefaBean.java
@@ -0,0 +1,33 @@
+package atividad;
+
+public class TarefaBean {
+
+	private String nome;
+	private double id;
+	private int porcentagem;
+	
+	public void setNome(String nome) {
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNumero(double id) {
+		
+	}
+	
+	public double getNumero(double id) {
+		return id;
+	}
+	
+	public Integer getPorcentagem(Integer porcentagem) {
+		return porcentagem;
+	}
+	
+	
+	public void setPorcentagem(Integer porcentagem) {
+		
+	}
+}
