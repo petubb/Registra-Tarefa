61a5fe02ea1ad8b11d7492779bdc45236df82ca0
petub <114745589+petubb@users.noreply.github.com>
2022-11-07 10:34:29 -0400

    tentativa de fazer um registro de tarefa
    tentativa de fazer um registro de tarefa

--- TarefaBean.java
+++ TarefaBean.java
@@ -1,13 +1,14 @@
-package atividad;
+package registraTarefa;
 
 public class TarefaBean {
 
 	private String nome;
 	private double id;
 	private int porcentagem;
-	
+	private boolean concluida;
+
 	public void setNome(String nome) {
-		
+
 	}
 
 	public String getNome() {
@@ -15,19 +16,27 @@ public class TarefaBean {
 	}
 
 	public void setNumero(double id) {
-		
+
 	}
-	
+
 	public double getNumero(double id) {
 		return id;
 	}
-	
+
 	public Integer getPorcentagem(Integer porcentagem) {
 		return porcentagem;
 	}
-	
-	
+
+
 	public void setPorcentagem(Integer porcentagem) {
-		
+
+	}
+
+	public boolean isConcluida() {
+		return concluida;
+	}
+	
+	public void setConcluida(boolean concluida) {
+		this.concluida = concluida;
 	}
 }
